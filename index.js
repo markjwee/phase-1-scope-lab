0a04906520688f5947110b16f6ac1cd1491bbfb7
Mark <mark.j.wee@gmail.com>
2021-12-16 11:50:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    var bestCustomer = `not bob`;
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = `maybe bob`;
+}
+
+const leastFavoriteCustomer = `inigo montoya`;
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = `mandy patinkin`
+}
